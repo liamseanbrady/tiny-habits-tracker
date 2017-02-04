1203225b8459e2a60bd9186cc10f36817321f3d4
Liam Sean Brady <liamseanbrady@gmail.com>
2017-02-04 20:27:29 +0000

    A quick end-to-end test which hits a real SQLite3 database as a sense-check, and cleans up after itself appropriately.

---
+++ end_to_end_test.py
@@ -0,0 +1,18 @@
+import unittest
+import os.path
+from tracker import Tracker
+from tracker import DB
+
+class EndToEndTest(unittest.TestCase):
+  def test_creating_a_db_and_adding_a_habit(self):
+    self.assertFalse(os.path.isfile("./db/tiny_habits_tracker"))
+    tracker = Tracker()
+    tracker.create_db()
+    self.assertTrue(os.path.isfile("./db/tiny_habits_tracker"))
+    tracker.add_habit("breathe")
+    self.assertEqual(tracker.habits()[0][1], "breathe")
+    os.remove("./db/tiny_habits_tracker")
+    self.assertFalse(os.path.isfile("./db/tiny_habits_tracker"))
+
+if __name__ == "__main__":
+  unittest.main()
