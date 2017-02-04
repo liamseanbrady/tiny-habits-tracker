0d37e5c67de11a80ca714234936478d383c5858a
Liam Sean Brady <liamseanbrady@gmail.com>
2017-02-04 20:16:58 +0000

    Make sure that if no DB adapter is specified is specified, then a default DB adapter is user. At the moment, the feature of being able to 'plug in' an adapter for persistence is mostly useful for testing and keeping the code modular. In the future, I anticipate wanting to try out different modes of persistence.

--- tracker.py
+++ tracker.py
@@ -1,6 +1,8 @@
 class Tracker:
   def __init__(self, **kwargs):
-    self.db = kwargs["db"]
+    self.db = kwargs.get("db")
+    if self.db == None:
+      self.db = DB()
 
   def has_existing_db(self):
     return self.db.exist("tiny_habits_tracker")
